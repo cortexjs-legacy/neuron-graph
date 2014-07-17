d21ec85b36b09ee5e4733621ae32a1f28096ef02
Wei Gao <jky239@gmail.com>
2014-07-17 19:20:55 +0800

    add unit tests

---
+++ test/test.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var expect = require('chai').expect;
+var graph = require('../');
+var fixture = require('test-fixture')();
+var jf = require('jsonfile');
+
+var cases = {
+  full: {
+    types: [
+      'devDependencies',
+      'asyncDependencies',
+      'dependencies',
+      'engines'
+    ],
+    e: function (tree, e) {
+      expect(tree).to.deep.equal(e);
+    }
+  },
+  pro: {
+    types: [
+      'asyncDependencies',
+      'dependencies',
+      'engines'
+    ],
+    e: function (tree, e) {
+      expect(tree).to.deep.equal(e);
+    }
+  },
+  pro_no_engine: {
+    tree: [
+      'asyncDependencies',
+      'dependencies'
+    ],
+    e: function (tree, e) {
+      expect(tree).to.deep.equal(e);
+    }
+  }
+};
+
+var shrinkwrap = jf.readFileSync(fixture.resolve('shrink.json'));
+var expected = require(fixture.resolve('expected'));
+var pkg = {
+  "name": "unit-m-customer",
+  "version": "0.0.0"
+};
+
+Object.keys(cases).forEach(function (type) {
+  var c = cases[type];
+  describe(type, function(){
+    it("graph()", function(){
+      graph(pkg, {
+        shrinkwrap: shrinkwrap,
+        dependencyKeys: c.types
+      }, function(err, t) {
+        console.log(t);
+      });
+//      c.e(t, expected[type]);
+    });
+  });
+});
+
